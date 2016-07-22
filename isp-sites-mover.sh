e43cde3d19a296f4d7645d371f3a12eacd44af84
lihachev <lihachev@fastvps.ru>
2016-07-22 13:17:11 +0300

    First usable version

--- isp-sites-mover.sh
+++ isp-sites-mover.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
-set -u
+set -ueo pipefail
 
 # Usage text
 usage_text=$(cat << EOF
-Usage: `basename $0`
+Usage: `basename $0` [-u user -d domain | -a]
 Script to move sites between ISPmanager users.
+
+You can move one site:
+    -u\tusername of new site owner
+    -d\tdomainname to move
+
+Or you can perform automatic moving of all sites to separate users:
+    -a\tmove all sites to separate users
+    -u\tmove sites only from one user
+    -x\texclude site (if you want to specify, which one to keep on old user)
+
+Users are created if do not exist.
 
 EOF
 )
@@ -14,24 +25,54 @@ TXT_GRN='\e[0;32m'
 TXT_RED='\e[0;31m'
 TXT_YLW='\e[0;33m'
 TXT_RST='\e[0m'
+DATE_FORMAT='+%H:%M:%S'
 
 TMP_PATH='/root/site_mover'
 DOMAIN_PARAMS=''
 ISP_VERSION=''
+ISPMGR_CONF=''
+MASS_MOVING='0'
+USERNAME=''
+DOMAIN=''
+EXCLUDE=''
+
+# Echo with time
+echo_time()
+{
+    echo -e "[${TXT_GRN}`date $DATE_FORMAT`${TXT_RST}] $1"
+}
+
+# Echo with leading TAB
+echo_tab()
+{
+    echo -e " - $1"
+}
 
 # Parsing arguments recieved
-check_args()
+parse_args()
 {
-    #if [ $# -eq 0 ]; then
-    #    echo -e "$usage_text"
-    #    exit 0
-    #fi
     local opt
-    while getopts "hc:" opt; do
+    if [ $# -eq 0 ]; then
+        echo -e "$usage_text"
+        exit 0
+    fi
+    while getopts "hu:d:x:a" opt; do
         case $opt in
             h )
                 echo -e "$usage_text"
                 exit 0
+            ;;
+            u )
+                USERNAME=$OPTARG
+            ;;
+            d )
+                DOMAIN=$OPTARG
+            ;;
+            a )
+                MASS_MOVING='1'
+            ;;
+            x )
+                EXCLUDE=$OPTARG
             ;;
             \? )
                 echo "Invalid option: -$OPTARG" >&2
@@ -41,7 +82,7 @@ check_args()
                 echo "Option -$OPTARG requires an argument." >&2
                 exit 1
             ;;
-      esac
+        esac
     done
 }
 
@@ -129,19 +170,19 @@ detect_os()
         local version=`grep '^VERSION_ID=' $os_release_file | awk -F'[". ]' '{print $2}'`
         OS=`echo "${name}${version}"`
         verify_package_manager $OS
-        echo -e "OS:\t${TXT_YLW}${OS}${TXT_RST}"
+        echo -e "OS: ${TXT_YLW}${OS}${TXT_RST}"
     else
         # If not, trying redhat-release file (mainly because of bitrix-env)
         if [ -f $redhat_release_file ]; then
             OS=`head -1 /etc/redhat-release | sed -re 's/([A-Za-z]+)[^0-9]*([0-9]+).*$/\1\2/'`
             verify_package_manager $OS
-            echo -e "OS:\t${TXT_YLW}${OS}${TXT_RST}"        
+            echo -e "OS: ${TXT_YLW}${OS}${TXT_RST}"        
         else
             # Else, trying issue file
             if [ -f $issue_file ]; then
                 OS=`head -1 $issue_file | sed -re 's/([A-Za-z]+)[^0-9]*([0-9]+).*$/\1\2/'` 
                 verify_package_manager $OS
-                echo -e "OS:\t${TXT_YLW}${OS}${TXT_RST}"
+                echo -e "OS: ${TXT_YLW}${OS}${TXT_RST}"
             else
                 # If none of that files worked, exit
                 echo -e "${TXT_RED}Cannot detect OS. Exiting now"'!'"${TXT_RST}"
@@ -186,8 +227,8 @@ detect_isp_version()
             finish NOTOK
         ;;
         1 )
-            echo "ISPmanager $version detected."
-            echo "Full version: $full_version"
+            echo -e "${TXT_YLW}ISPmanager ${version}${TXT_RST} detected."
+            echo -e "Full version: ${TXT_YLW}${full_version}${TXT_RST}"
         ;;
         * )
             echo "Can't detect ISPmanager version"
@@ -205,11 +246,20 @@ set_commands()
 
         ;;
         5 )
+            ISPMGR_CONF='/usr/local/mgr5/etc/ispmgr.conf'
+            kill_ispmgr_command='killall -9 core'
+
             create_user_command='/usr/local/mgr5/sbin/mgrctl -m ispmgr user.add.finish addinfo=on confirm=* ftp_inaccess= ftp_user=on ftp_user_name=$username limit_charset=off limit_db_enabled=on limit_dirindex="index.php index.html" limit_ftp_users= limit_ftp_users_inaccess= limit_php_mode=php_mode_mod limit_php_mode_cgi=on limit_php_mode_mod=on name=$username passwd=$password php_enable=on sok=ok'
             create_site_command='/usr/local/mgr5/sbin/mgrctl -m ispmgr webdomain.edit $domain_params'
             gen_siteparams_command='isp5_gen_siteparams $domain $username'
             remove_site_command='/usr/local/mgr5/sbin/mgrctl -m ispmgr webdomain.delete elid=$domain'
             get_domain_dir="sqlite3 /usr/local/mgr5/etc/ispmgr.db \"SELECT docroot FROM webdomain WHERE name='\$domain';\""
+            check_user_command='/usr/local/mgr5/sbin/mgrctl -m ispmgr user | grep -q "^name=$username"; echo $?'
+            check_domain_command='/usr/local/mgr5/sbin/mgrctl -m ispmgr webdomain | grep -q "^name=$domain"; echo $?'
+            check_domain_owner_command='/usr/local/mgr5/sbin/mgrctl -m ispmgr webdomain | grep -q "^name=$domain owner=$username"; echo $?'
+
+            get_all_users_command='/usr/local/mgr5/sbin/mgrctl -m ispmgr user | sed -re "s/^name=([^ ]+) .*$/\1/"'
+            get_all_sites_by_user_command='/usr/local/mgr5/sbin/mgrctl -m ispmgr webdomain | grep "owner=$username" | sed -re "s/^name=([^ ]+) .*$/\1/"'
 
         ;;
         * )
@@ -225,19 +275,91 @@ move_site()
 {
     local domain=$1
     local username=$2
+    local result=''
 
-    #eval $gen_siteparams_command
-    isp5_gen_siteparams $domain $username
-    echo "Creating user $username..."
-    create_user $username
-    echo "Backing up docroot for $domain"
-    backup_domain_dir $domain
-    echo "Removing old $domain"
-    remove_site $domain
-    echo "Creating new $domain"
-    create_site $domain $DOMAIN_PARAMS
-    echo "Restoring docroot for $domain"
-    restore_domain_dir $domain
+    local check_result=$(check_input $domain $username)
+    case $check_result in
+        # Domain exists, used does not, full action set
+        100 )
+            echo_tab "Getting current site parameters"
+            eval $gen_siteparams_command
+
+            echo_tab "Creating user $username"
+            create_user $username
+
+            echo_tab "Backing up docroot for $domain"
+            backup_domain_dir $domain
+
+            echo_tab "Removing old $domain from panel"
+            remove_site $domain
+
+            echo_tab "Creating new $domain in panel"
+            create_site $domain
+
+            echo_tab "Restoring docroot for $domain"
+            restore_domain_dir $domain
+
+            echo_tab "Setting permissions for $domain"
+            set_permissions $domain $username
+        ;;
+        # User and domain exist. no need to create user
+        110 )
+            echo_tab "Getting current site parameters"
+            eval $gen_siteparams_command
+
+            echo_tab "User $username exists."
+
+            echo_tab "Backing up docroot for $domain"
+            backup_domain_dir $domain
+
+            echo_tab "Removing old $domain from panel"
+            remove_site $domain 
+
+            echo_tab "Creating new $domain in panel"
+            create_site $domain
+
+            echo_tab "Restoring docroot for $domain"
+            restore_domain_dir $domain
+
+            echo_tab "Setting permissions for $domain"
+            set_permissions $domain $username
+        ;;
+        # Everything is already done
+        111 )
+            echo_tab "Domain '$domain' already belongs to user '$username'. Skipping."
+            return 0
+        ;;
+        # Domain does not exist
+        0?? )
+            echo_tab "Domain '$domain' does not exist."
+        ;;
+        * )
+            echo_tab 'How did you get there?'
+            finish NOTOK
+        ;;
+    esac
+}
+
+# Check input for correctness
+check_input()
+{
+    local domain=$1
+    local username=$2
+    local domain_exists='0'
+    local user_exists='0'
+    local needed_owner='0'
+    if [ `eval $check_domain_command` -eq 0 ]; then
+        domain_exists='1'
+    fi
+    if [ `eval $check_user_command` -eq 0 ]; then
+        user_exists='1'
+    fi
+    if [ `eval $check_domain_owner_command` -eq 0 ]; then
+        needed_owner='1'
+    fi
+    
+    local result=`echo "${domain_exists}${user_exists}${needed_owner}"`
+    echo $result
 }
 
 # Generate site parameters
@@ -260,12 +382,26 @@ create_user()
 {
     local username=$1
     local password=`pwgen -scan 16 1`
+    local func_name=${FUNCNAME[0]}
 
-    local result=`eval $create_user_command`
-    #if [[ $result =~ ERROR ]]; then
-    #    echo $result
-    #    finish NOTOK
-    #fi
+    local answer=`eval $create_user_command`
+    check_result $func_name ${answer[@]}
+    local result=$?
+    case $result in
+        0 )
+            return 0
+        ;;
+        1 )
+            echo "Error in $func_name"
+            echo "$answer"
+            return 1
+        ;;
+        * )
+            echo "Undefined result in $func_name "
+            echo -e "Answer:\n$answer"
+            finish NOTOK
+        ;;
+    esac
 }
 
 # Create www-domain with ISPmanager
@@ -273,24 +409,85 @@ create_site()
 {
     local domain=$1
     local domain_params=$DOMAIN_PARAMS
+    local func_name=${FUNCNAME[0]}
 
-    local result=`eval $create_site_command`
-    if [[ $result =~ ERROR ]]; then
-        echo $result
-        finish NOTOK
-    fi
+    local answer=`eval $create_site_command`
+    check_result $func_name ${answer[@]}
+    local result=$?
+    case $result in
+        0 )
+            return 0
+        ;;
+        1 )
+            echo "Error in $func_name"
+            echo "$answer"
+            return 1
+        ;;
+        2 )
+            echo "Adding 'Option InsecureDomain' to $ISPMGR_CONF"
+            echo 'Option InsecureDomain' >> $ISPMGR_CONF
+            eval $kill_ispmgr_command
+            answer=`eval $create_site_command`
+        ;;
+        * )
+            echo "Undefined result in $func_name "
+            echo -e "Answer:\n$answer"
+            finish NOTOK
+        ;;
+    esac
+}
+
+# Check ISPmanager API results
+check_result()
+{
+    local func=$1
+    shift
+    local answer=$@
+
+    case $func in
+        create_site )
+            if [[ $answer =~ 'ERROR dns(domain_access)' ]]; then
+                return 2
+            elif [[ $answer =~ 'ERROR' ]]; then
+                return 1
+            else
+                return 0
+            fi
+        ;;
+        remove_site )
+            if [[ $answer =~ 'ERROR' ]]; then
+                return 1
+            else
+                return 0
+            fi
+        ;;
+    esac
 }
 
 # Remove www-domain with ISPmanager
 remove_site()
 {
     local domain=$1
+    local func_name=${FUNCNAME[0]}
 
-    local result=`eval $remove_site_command`
-    if [[ $result =~ ERROR ]]; then
-        echo $result
-        finish NOTOK
-    fi
+    local answer=`eval $remove_site_command`
+    check_result $func_name ${answer[@]}
+    local result=$?
+    case $result in
+        0 )
+            return 0
+        ;;
+        1 )
+            echo "Error in $func_name"
+            echo "$answer"
+            return 1
+        ;;
+        * )
+            echo "Undefined result in $func_name "
+            echo -e "Answer:\n$answer"
+            finish NOTOK
+        ;;
+    esac
 }
 
 # Move sitedir to tmp
@@ -318,14 +515,129 @@ restore_domain_dir()
     mv $backup_path $domain_dir
 }
 
+# Fix owner for sitedir
+set_permissions()
+{
+    local domain=$1
+    local username=$2
+
+    local domain_dir=`eval $get_domain_dir`
+    chown -R ${username}:${username} $domain_dir
+}
 
 
 
-check_args
+# Move all sites to different users
+move_all_sites()
+{
+    local domain=''
+    local username=''
+    local new_username=''
+    local users=''
+    local domains=''
+    local exclude=$EXCLUDE
+
+    if [ $# -eq 0 ]; then
+        users=(`eval $get_all_users_command`)
+    else
+        users=$1
+    fi
+
+    for username in ${users[@]}; do
+        echo_time "Processing user '$username'"
+        if [ `eval $check_user_command` -eq 1 ]; then
+            echo "No user '$username'. Skipping."
+            break 
+        fi
+        domains=(`eval $get_all_sites_by_user_command`)
+        if [ ${#domains[@]} -gt 1 ]; then
+            for domain in ${domains[@]}; do
+                if [ "$domain" == "$exclude" ]; then
+                    echo_time "Site '$domain' is exluded. Skipping."
+                else
+                    new_username=$(gen_new_usename $domain)
+                    echo_time "Moving site '$domain' to user '$new_username'"
+                    move_site $domain $new_username
+                fi
+            done
+        else
+            echo "User '$username' have only ${#domains[@]} sites. Skipping."
+        fi
+    done
+    echo_time 'Finished!'
+}
+
+# Generate new username based on domain
+gen_new_usename()
+{
+    local domain=$1
+    local username=''
+    
+    # Form username from domain
+    # Main idea is create something like my_domain_com or my_domain_com_3
+    # We are limited to 16 characters and chop trailing '_' just for more fancy names
+    local dot_count=`echo $domain | grep -o '\.' | wc -l`
+    case $dot_count in
+        # Not a correct domain
+        0 )
+            finish "Not a correct domain name: '$domain'."
+        ;;
+        # 2nd level domain
+        1 )
+            username=`echo $domain | awk -F\. '{print $(NF-1)"_"$NF}' | cut -c1-13 | sed -e 's/_$//'`
+        ;;
+        # 3rd level domain
+        2 )
+            username=`echo $domain | awk -F\. '{print $(NF-2)"_"$(NF-1)"_"$NF}' | cut -c1-13 | sed -e 's/_$//'`
+        ;;
+        # More level domain
+        * )
+            username=`echo $domain | awk -F\. '{print $1_$2}' | cut -c1-13 | sed -e 's/_$//'`
+        ;;      
+    esac
+
+    # Add 01-99 to the end, if user exists
+    if [ `eval $check_user_command` -eq 0 ]; then
+        local short_username=$username
+        for i in {1..99}; do
+            username="${short_username}_${i}"
+            if [ `eval $check_user_command` -ne 0 ]; then 
+                break
+            elif [ $i -eq 99 ]; then 
+                finish "Can't create user for domain '$domain'"
+            fi
+        done
+
+    fi
+    echo $username
+}
+
+
+
+# Preparing everything
+parse_args "$@"
 check_bash_version
 detect_package_manager
 detect_os
 detect_isp_version
 set_commands
 
-move_site drupal.lilal.tk fasttest
+# Move sites
+if [ $MASS_MOVING -eq 0 ]; then
+    if [ ! -z $DOMAIN ] || [ ! -z $USERNAME ]; then
+        echo_time "Moving site '$DOMAIN' to user '$USERNAME'"
+        move_site $DOMAIN $USERNAME
+        echo_time 'Finished!'
+    else
+        echo -e "$usage_text"
+        exit 1
+    fi
+else
+    if [ ! -z $USERNAME ]; then
+        echo "Moving all sites of user '$USERNAME' to new users."
+        move_all_sites $USERNAME
+    else
+        echo "Moving all sites to new users."
+        move_all_sites
+    fi
+fi
